a815c19be150a67f013703c3f4d3f78164f5db6a
Navya-Mini <navyarukmini.koteru@valuemomentum.com>
2022-04-13 15:02:57 +0530

    first mobile test

---
+++ Test Suites/Mobile_Test.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mobile_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1709c0d3-160b-4c9a-8ead-47b4b9906233</testSuiteGuid>
+   <testCaseLink>
+      <guid>b3d36d08-66e2-4a1d-a061-958aca3cf44e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
